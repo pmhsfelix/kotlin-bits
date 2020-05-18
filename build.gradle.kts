a886f173693694b44fdbd7f41adf45772ec7d84f
Pedro Felix <pmhsfelix@gmail.com>
2020-05-18 17:50:21 +0100

    changes dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,10 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
+    implementation("org.slf4j:slf4j-api:1.7.30")
     testImplementation("junit:junit:4.+")
+    testImplementation("org.slf4j:slf4j-simple:1.7.30")
 }
 
 tasks.withType<KotlinCompile> {
